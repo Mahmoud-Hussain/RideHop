f4b4c83259d00eda60b3bd637fd6fd62656c9e91
Mahmoud-Hussain <121444044+Mahmoud-Hussain@users.noreply.github.com>
2023-06-24 03:41:33 +0600

    first Update

--- src/main/java/app/login.java
+++ src/main/java/app/login.java
@@ -3,6 +3,8 @@ package app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -11,9 +13,11 @@ public class login extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("loginController.fxml")));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), Color.WHITE);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
     }
 }
